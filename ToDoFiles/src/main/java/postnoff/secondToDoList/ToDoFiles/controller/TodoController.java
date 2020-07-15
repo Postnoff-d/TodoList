7c87524f694ca3c97c00af2690b8d883586447e6
Denis <postnoff.d@yahoo.com>
2020-07-15 17:23:39 -0400

    small updates for "delete"

--- ToDoFiles/src/main/java/postnoff/secondToDoList/ToDoFiles/controller/TodoController.java
+++ ToDoFiles/src/main/java/postnoff/secondToDoList/ToDoFiles/controller/TodoController.java
@@ -8,18 +8,14 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import postnoff.secondToDoList.ToDoFiles.model.TodoItem;
 import postnoff.secondToDoList.ToDoFiles.repo.TodoRepo;
  
 @RestController
 @RequestMapping(value = "/todo")
-public class TodoController {
+public class  TodoController {
 	
 	@Autowired
 	private TodoRepo todoRepo;
@@ -32,6 +28,16 @@ public class TodoController {
 	@PostMapping
 	public TodoItem save(@Valid @NotNull @RequestBody TodoItem todoItem) {
 		return todoRepo.save(todoItem);
+	}
+
+	@PutMapping
+	public TodoItem update(@Valid @NotNull @RequestBody TodoItem todoItem) {
+		return todoRepo.save(todoItem);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public void delete(@PathVariable Long id) {
+		todoRepo.deleteById(id);
 	}
 
 }
